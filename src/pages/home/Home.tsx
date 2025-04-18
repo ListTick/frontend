b83773a2e908022cc0e609708521487ec49b5f48
przemyslawren <przemyslaw.ren@gmail.com>
2025-04-19 00:02:23 +0200

    feat: welcome user on visit

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,16 @@
+import { useCurrentUser } from '../../hooks/useCurrentUser.ts';
+import dayjs from 'dayjs';
+import './Home.scss';
+
 const Home = () => {
-  return <div>Home</div>;
+  const user = useCurrentUser();
+
+  return (
+    <div className='home'>
+      <h2>Hello, {user.profile?.firstName}</h2>
+      <p className='home__date'>Today, {dayjs().format('DD MMM')}</p>
+    </div>
+  );
 };
 
 export default Home;
