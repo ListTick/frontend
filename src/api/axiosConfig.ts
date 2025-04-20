576bf71947694592c68a1ef7dbc0d3a937b84166
Milosz Pankanin <pankanin.milosz@gmail.com>
2025-04-20 14:53:54 +0200

    add axios requests logic

---
+++ src/api/axiosConfig.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosInstance } from 'axios';
+import useKeycloak from '../hooks/useKeycloak.ts';
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: 'http://localhost:8080',
+  timeout: 10_000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+const keycloak = useKeycloak();
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    if (keycloak.authenticated) {
+      config.headers.Authorization = `Bearer ${keycloak.token}`;
+    } else {
+      keycloak.login();
+    }
+    return config;
+  }
+);
+
+export default axiosInstance;
