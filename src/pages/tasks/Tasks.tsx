6cbc6e3813ac99d7a8ee6483629d5266d8d7e23c
przemyslawren <przemyslaw.ren@gmail.com>
2025-04-20 22:29:00 +0200

    feat: create test api call

--- src/pages/tasks/Tasks.tsx
+++ src/pages/tasks/Tasks.tsx
@@ -1,5 +1,14 @@
+import { useAxios } from '../../config/axios.ts';
+import { useQuery } from '@tanstack/react-query';
+
 const Tasks = () => {
-  return <div>Tasks</div>;
+  const api = useAxios();
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: () => api.get('/account/get').then((res) => res.data)
+  });
+  return <div>{data}</div>;
 };
 
 export default Tasks;
