71b4890c44362c434781e0f90ab439d77dc297b1
przemyslawren <przemyslaw.ren@gmail.com>
2025-04-21 12:54:09 +0200

    feat: create mock tasks component

--- src/pages/tasks/Tasks.tsx
+++ src/pages/tasks/Tasks.tsx
@@ -1,14 +1,24 @@
-import { useAxios } from '../../config/axios.ts';
 import { useQuery } from '@tanstack/react-query';
+import { getTasks } from '../../api/tasks.ts';
 
 const Tasks = () => {
-  const api = useAxios();
-
-  const { data, error, isLoading } = useQuery({
+  const { data } = useQuery({
     queryKey: ['tasks'],
-    queryFn: () => api.get('/account/get').then((res) => res.data)
+    queryFn: () => getTasks()
   });
-  return <div>{data}</div>;
+
+  return (
+    <div className='tasks'>
+      <h1>Tasks</h1>
+      <div className='tasks__list'>
+        {data?.map((task: any) => (
+          <div key={task.id} className='tasks__item'>
+            <h2>{task}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Tasks;
