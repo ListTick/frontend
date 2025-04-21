3637b451d64d5daae56b88901b62aa185b094115
przemyslawren <przemyslaw.ren@gmail.com>
2025-04-21 12:53:50 +0200

    feat: update axios

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,24 +1,22 @@
-import axios, { AxiosInstance, AxiosRequestHeaders } from 'axios';
-import useKeycloak from '../hooks/useKeycloak.ts';
-import { useMemo } from 'react';
+import axios, { AxiosRequestHeaders } from 'axios';
+import { keycloak } from '../security/keycloak.ts';
 
-export function useAxios(): AxiosInstance {
-  const keycloak = useKeycloak();
+export const api = axios.create({ baseURL: 'http://localhost:8080/api' });
 
-  return useMemo(() => {
-    const instance = axios.create({
-      baseURL: 'http://localhost:8080/api',
-      headers: { 'Content-Type': 'application/json' }
-    });
+api.interceptors.request.use(async (config) => {
+  await keycloak.updateToken(60).catch(() => keycloak.logout());
 
-    instance.interceptors.request.use(async (config) => {
-      await keycloak.updateToken(60).catch(() => keycloak.logout());
+  const headers = config.headers as AxiosRequestHeaders;
+  headers.Authorization = `Bearer ${keycloak.token}`;
+  return config;
+});
 
-      const headers = config.headers as AxiosRequestHeaders;
-      headers.Authorization = `Bearer ${keycloak.token}`;
-      return config;
-    });
-
-    return instance;
-  }, [keycloak]);
-}
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response.status === 401) {
+      await keycloak.logout();
+    }
+    return Promise.reject(error);
+  }
+);
