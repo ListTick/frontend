de27f03e1f444bbb7559768f6567028006a356e4
mpankanin <pankanin.milosz@gmail.com>
2025-08-01 09:16:17 +0200

    add motivational quotes

--- src/components/home/Intro/Intro.tsx
+++ src/components/home/Intro/Intro.tsx
@@ -1,10 +1,13 @@
 import dayjs from 'dayjs';
 import { KeycloakProfile } from 'keycloak-js';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import './Intro.scss'
+import { motivationalQuotes } from '@/components/home/Intro/MotivationQuotes.ts';
+
+'MotivationQuotes.ts'
 
 interface User {
   id: string | undefined;
@@ -18,18 +21,30 @@ interface IntroProps {
 }
 
 const Intro: React.FC<IntroProps> = ({ user }) => {
+  const getGreeting = () => {
+    const hour = new Date().getHours();
+    if (hour >= 5 && hour < 12) return 'Good morning';
+    if (hour >= 12 && hour < 17) return 'Good afternoon';
+    if (hour >= 17 && hour < 22) return 'Good evening';
+    return 'Good night';
+  };
+
+  const dayOfMonth = new Date().getDate();
+  const quoteOfTheDay = motivationalQuotes[(dayOfMonth - 1) % motivationalQuotes.length];
+
   return (
-    <div className="intro">
-      <div className="intro__content">
-        <h2>{`Hey ${user.profile?.firstName}`}</h2>
-        <div className="intro__content__calendar">
+    <div className='intro'>
+      <h2>{`${getGreeting()} ${user.profile?.firstName}`}</h2>
+      <div className='intro__content'>
+        <div className='intro__content__calendar'>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar
-              referenceDate={dayjs()}
-              views={['year', 'month', 'day']}
-            />
+            <DateCalendar referenceDate={dayjs()} views={['year', 'month', 'day']} />
           </LocalizationProvider>
         </div>
+        <div className='intro__content__quote'>
+          <p>{quoteOfTheDay}</p>
+        </div>
+
       </div>
     </div>
   );
