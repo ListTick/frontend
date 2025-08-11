da45d1e2411a2014a562765042649f9d7fa9ca72
przemyslawren <przemyslaw.ren@gmail.com>
2025-08-11 22:29:49 +0200

    feat: simplify KeycloakProvider.tsx setup

--- src/security/keycloak.ts
+++
@@ -1,7 +0,0 @@
-import Keycloak from 'keycloak-js';
-
-export const keycloak = new Keycloak({
-  url: '/auth',             // Nginx proxy → Keycloak
-  realm: 'listtick',
-  clientId: 'listtick-frontend',
-});
